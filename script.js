314fe4e3ad9d69596f1af11d04c84c860e250584
Mariam Bradai <mariambradai2@gmail.com>
2022-02-12 16:06:28 +0100

    fix reset buttons not working

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ class Counter extends React.Component {
         super(props)
         this.state = {
             count: 0,
-            totalTaps:0
+            totalTaps: 0
         }
 
     }
-   
+    handleDecrement = () => {
+        if (this.state.count === 0) {
+            this.setState({
+                totalTaps: this.state.totalTaps + 1
+            })
+            return;
+        }
+        this.setState({
+            count: this.state.count - 1,
+            taps: this.state.totalTaps + 1
+        })
+    }
+
+
+
 
     render() {
         return (
@@ -16,12 +30,12 @@ class Counter extends React.Component {
                 <h1 className="count">{this.state.count}</h1>
                 <h1 className="taps">Total taps {this.state.totalTaps}</h1>
                 <div>
-                <button onClick={() => this.setState({ count: this.state.count + 1 , totalTaps: this.state.count +1})}> increment +</button>
-                <button onClick={() => this.setState({ count: this.state.count - 1 })}> decremnt -</button>
+                    <button onClick={() => this.setState({ count: this.state.count + 1, totalTaps: this.state.count + 1 })}> increment +</button>
+                    <button onClick={this.handleDecrement}> decremnt -</button>
                 </div>
                 <div>
-                <button className="reset-count" onClick={() => this.setState({count :0})} >Reset Count</button>
-                <button className="reset-all" onClick={()=> this.setState({totalTaps :0 ,count:0})}>Reset All</button>
+                    <button className="reset-count" onClick={() => this.setState({ count: 0 })} >Reset Count</button>
+                    <button className="reset-all" onClick={() => this.setState({ totalTaps: 0, count: 0 })}>Reset All</button>
                 </div>
             </div>
         )
