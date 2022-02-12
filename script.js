33ce4e35bae5498c9109712307e7ad96694be7e8
Mariam Bradai <mariambradai2@gmail.com>
2022-02-12 15:59:05 +0100

    fix reset buttons not working

--- script.js
+++ script.js
@@ -2,26 +2,27 @@ class Counter extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            count: 0
+            count: 0,
+            totalTaps:0
         }
 
     }
-    componentWillUnmount() {
-        clearInterval(this.interval)
-    }
-
-    clear = () => {
-        clearInterval(this.interval)
-    }
+   
 
     render() {
         return (
             <div className="counter">
                 <h1>counter</h1>
-                <h1>{this.state.count}</h1>
-                <button onClick={() => this.setState({ count: this.state.count + 1 })}> increment +</button>
+                <h1 className="count">{this.state.count}</h1>
+                <h1 className="taps">Total taps {this.state.totalTaps}</h1>
+                <div>
+                <button onClick={() => this.setState({ count: this.state.count + 1 , totalTaps: this.state.count +1})}> increment +</button>
                 <button onClick={() => this.setState({ count: this.state.count - 1 })}> decremnt -</button>
-                <button className="reset-count" onClick={this.clear}>reset</button>
+                </div>
+                <div>
+                <button className="reset-count" onClick={() => this.setState({count :0})} >Reset Count</button>
+                <button className="reset-all" onClick={()=> this.setState({totalTaps :0 ,count:0})}>Reset All</button>
+                </div>
             </div>
         )
     }
